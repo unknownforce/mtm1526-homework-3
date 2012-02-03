735e5e43abf4b961866c5474149185b7cb390ccd
unknownforce <petchan34@gmail.com>
2012-02-03 10:06:46 -0500

    finished

--- js/jqueryapp.js
+++ js/jqueryapp.js
@@ -1,1 +1,65 @@
 // JavaScript Document
+
+
+$(document).ready(function() {
+	
+		$('#border').on('click', function (ev) {	
+			
+			$('form').on('submit', function (ev) {
+				ev.preventDefault();
+				//$('#inner').css('background-color', '#0F0');
+				//$('#outer').css('border-color', '#0F0');
+				if ($('#color').val() != '') {
+					
+					$('#outer').css('border-color', $('#color').val() );
+					//$(this).css('background-color', '#f33');
+				}
+				
+				
+			});
+		});
+		
+		$('#bg').on('click', function (ev) {	
+			
+			$('form').on('submit', function (ev) {
+				ev.preventDefault();
+				//$('#inner').css('background-color', '#0F0');
+				//$('#outer').css('border-color', '#0F0');
+				if ($('#color').val() != '') {
+					
+					$('#inner').css('background-color', $('#color').val() );
+					//$(this).css('background-color', '#f33');
+				}
+				
+				
+			});
+		});
+		
+		
+		$('#text').on('click', function (ev) {	
+			
+			$('form').on('submit', function (ev) {
+				ev.preventDefault();
+				//$('#inner').css('background-color', '#0F0');
+				//$('#outer').css('border-color', '#0F0');
+				if ($('#color').val() != '') {
+					
+					$('#textcolor').css('color', $('#color').val() );
+					//$(this).css('background-color', '#f33');
+				}
+				
+				
+			});
+		});
+	
+		$('button').on('click', function (ev) {	
+		
+			ev.preventDefault();
+			$('#outer').toggleClass('hide');
+		});
+	
+});
+	
+
+
+
